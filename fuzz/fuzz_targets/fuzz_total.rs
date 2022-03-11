2278843bee13bb5d8c7cf224457f2f9cdd616350
Ben-Lichtman <Ben_Lichtman@icloud.com>
2022-03-11 11:33:10 -0800

    added fuzzing harness for cargo-fuzz

---
+++ fuzz/fuzz_targets/fuzz_total.rs
@@ -0,0 +1,27 @@
+#![no_main]
+use cfb::CompoundFile;
+use libfuzzer_sys::fuzz_target;
+use std::io::{Cursor, Read};
+
+fuzz_target!(|data: &[u8]| {
+    let cursor = Cursor::new(data);
+    let mut cfb = match CompoundFile::open(cursor) {
+        Ok(cfb) => cfb,
+        Err(_) => return,
+    };
+    let stream_paths = cfb
+        .walk()
+        .filter(|e| e.is_stream())
+        .map(|e| e.path().to_path_buf())
+        .collect::<Vec<_>>();
+
+    let paths = stream_paths
+        .into_iter()
+        .map(|s| {
+            let mut data = Vec::new();
+            cfb.open_stream(&s)?.read_to_end(&mut data)?;
+            Ok((s, data))
+        })
+        .collect::<Result<std::collections::HashMap<_, _>, std::io::Error>>()
+        .unwrap();
+});
